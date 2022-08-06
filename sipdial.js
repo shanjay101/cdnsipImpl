c9d16f9da9ce39174baf4fa3bfc545296983c540
shanjay101 <84106704+shanjay101@users.noreply.github.com>
2022-08-06 18:04:41 +0500

    Update sipdial.js
    syntex error check

--- sipdial.js
+++ sipdial.js
@@ -20,18 +20,13 @@ async function wait(ms: number): Promise<void> {
 // Main function
 async function main(): Promise<void> {
   const server = "wss://3frontoffice.nr.tre.se";
-
   //const destination = "sip:+923235037548@3kontaktpartnernr.dk";
   const destination = "sip:00923235037548@3kontaktpartnernr.dk";
-
   const aor = "sip:device.a23995751@3kontaktpartnernr.dk";
-
   const authorizationUsername = "device.a23995751";
-  
   const authorizationPassword = "Arooj123";
 
-
-const options: SimpleUserOptions = {
+  const options: SimpleUserOptions = {
     aor,
     media: {
       remote: {
@@ -44,10 +39,7 @@ const options: SimpleUserOptions = {
     }
   };
 
-  // Construct a SimpleUser instance
   const simpleUser = new SimpleUser(server, options);
-
-  // Supply delegate to handle inbound calls (optional)
   simpleUser.delegate = {
     onCallReceived: async () => {
       await simpleUser.answer();
@@ -69,14 +61,6 @@ const options: SimpleUserOptions = {
     console.log(error)
     
   }
- 
-  
-
-  // Wait some number of milliseconds
-  //await wait(5000);
-
-  // Hangup call
-  //await simpleUser.hangup();
 }
 
 // Run it
